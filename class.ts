02daf8c868acce3fa44d79f5b1e61ed934085ca3
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-10-25 11:55:32 +0900

    get set

--- class.ts
+++ class.ts
@@ -11,13 +11,27 @@ class Person1 {
 }
 
 class Teacher extends Person1 {
-  constructor(name: string, age: number, private subject: string) {
+  // get accessor
+  get subject() {
+    if (!this._subject) {
+      throw new Error('There is no subject.');
+    }
+    return 'fromGet Music'
+  }
+  // setでは処理が呼び出されたタイミングで下記の処理を実行
+  set subject(value) {
+    this._subject = value;
+  }
+  constructor(name: string, age: number, private _subject: string) {
     super(name, age)
   }
   sayHi() {
-    console.log(`hello ${ this.name }. im ${ this.age } years old. i teach ${ this.subject }`);
+    console.log(`hello ${ this.name }. im ${ this.age } years old. i teach ${ this._subject }`);
   }
 }
 
 const teacher = new Teacher('yuji', 32, 'Math');
+
+teacher.subject = 'sample';
+console.log(teacher.subject);
 teacher.sayHi();
