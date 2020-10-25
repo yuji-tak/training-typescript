4577c7b459443ee7e456d98b052ede8d9383f8b3
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-10-25 11:01:14 +0900

    private modifier

--- class.ts
+++ class.ts
@@ -1,30 +1,20 @@
 class Person1 {
   // フィールド
   name: string;
+  private age: number;
 
-  constructor(initName: string) {
+  constructor(initName: string, initAge: number) {
     this.name = initName;
+    this.age = initAge;
   }
 
-  // TSとES6の書き方は同じ
-  // 第一引数でthisの型定義のを行う、第二引数以降では通常通り引数をとることができる
-  // sayHi(this: { name: string }) {
-  // ↓更にTSにおいて、classで定義したインスタンスが表す型をもつ
+  increment() {
+    this.age += 1;
+  }
   sayHi(this: Person1) {
     console.log(`hello ${ this.name }`);
   }
-
-  // アロー関数では呼び出し時ではなく、定義時にthisを定義できるので下記のように書き換えができるが、この場合はこのclassで指定したnameプロパティを変更することができない
-  // sayHi = () => {
-  //   console.log(`hello ${ this.name }`);
-  // }
 }
-const test = new Person1('yuji');
+const test = new Person1('yuji', 32);
 console.log(test);
 test.sayHi();
-
-const anotherTest = {
-  name: 'anotherTest',
-  sayHi: test.sayHi
-}
-anotherTest.sayHi();
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,1 @@
 "use strict";
-var test = 'hi';
-console.log(test);
