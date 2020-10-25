07aad226729e81223f7124714f0310fd926895dc
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-10-25 11:19:56 +0900

    readonly

--- class.ts
+++ class.ts
@@ -1,20 +1,13 @@
 class Person1 {
-  // フィールド
-  name: string;
-  private age: number;
-
-  constructor(initName: string, initAge: number) {
-    this.name = initName;
-    this.age = initAge;
+  constructor(public readonly name: string, public age: number) {
   }
 
   increment() {
-    this.age += 1;
+    return this.age += 1;
   }
   sayHi(this: Person1) {
     console.log(`hello ${ this.name }`);
   }
 }
 const test = new Person1('yuji', 32);
-console.log(test);
 test.sayHi();
