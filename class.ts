f9806c4a031cf291a8c4930d260b1fa21d307951
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-10-25 11:30:37 +0900

    extends super

--- class.ts
+++ class.ts
@@ -6,8 +6,18 @@ class Person1 {
     return this.age += 1;
   }
   sayHi(this: Person1) {
-    console.log(`hello ${ this.name }`);
+    console.log(`hello ${ this.name }. im ${ this.age } years old`);
   }
 }
-const test = new Person1('yuji', 32);
-test.sayHi();
+
+class Teacher extends Person1 {
+  constructor(name: string, age: number, private subject: string) {
+    super(name, age)
+  }
+  sayHi() {
+    console.log(`hello ${ this.name }. im ${ this.age } years old`);
+  }
+}
+
+const teacher = new Teacher('yuji', 32, 'Math');
+teacher.sayHi();
