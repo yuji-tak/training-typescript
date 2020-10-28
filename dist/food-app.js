3a0f60d2f08a06f734b0115de3eb11a590714be7
yuji-tak <58871868+yuji-tak@users.noreply.github.com>
2020-10-28 14:46:57 +0900

    activeElementsScore

--- dist/food-app.js
+++ dist/food-app.js
@@ -18,10 +18,42 @@ var Food = /** @class */ (function () {
 var Foods = /** @class */ (function () {
     function Foods() {
         this.elements = document.querySelectorAll('.food');
+        this._activeElements = [];
+        this._activeElementsScore = [];
         this.elements.forEach(function (element) {
             new Food(element);
         });
     }
+    Object.defineProperty(Foods.prototype, "activeElements", {
+        // get構文は、オブジェクトのプロパティに関数を結びつけ、プロパティが参照された時に関数が呼び出されるようする
+        get: function () {
+            var _this = this;
+            this._activeElements = [];
+            this.elements.forEach(function (element) {
+                if (element.classList.contains('food--active')) {
+                    _this._activeElements.push(element);
+                }
+            });
+            return this._activeElements;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Foods.prototype, "activeElementsScore", {
+        get: function () {
+            var _this = this;
+            this._activeElementsScore = [];
+            this.activeElements.forEach(function (element) {
+                var foodScore = element.querySelector('.food__score');
+                if (foodScore) {
+                    _this._activeElementsScore.push(Number(foodScore.textContent));
+                }
+            });
+            return this._activeElementsScore;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Foods;
 }());
 var foods = new Foods();
--- food-app.ts
+++ food-app.ts
@@ -12,6 +12,31 @@ class Food {
 
 class Foods {
   elements = document.querySelectorAll<HTMLDivElement>('.food');
+
+  private _activeElements: HTMLDivElement[] = [];
+  private _activeElementsScore: number[] = [];
+  // get構文は、オブジェクトのプロパティに関数を結びつけ、プロパティが参照された時に関数が呼び出されるようする
+  get activeElements() {
+    this._activeElements = [];
+    this.elements.forEach(element => {
+      if (element.classList.contains('food--active')) {
+        this._activeElements.push(element);
+      }
+    })
+    return this._activeElements;
+  }
+
+  get activeElementsScore() {
+    this._activeElementsScore = [];
+    this.activeElements.forEach(element => {
+      const foodScore = element.querySelector('.food__score');
+      if (foodScore) {
+        this._activeElementsScore.push(Number(foodScore.textContent));
+      }
+    })
+    return this._activeElementsScore;
+  }
+
   constructor() {
     this.elements.forEach(element => {
       new Food(element);
